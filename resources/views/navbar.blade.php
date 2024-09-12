f9dafc5d430ffae96658b1dd6a382305e4dcef39
Lorenzo-Mariano <hhumminah@gmail.com>
2024-09-12 20:17:00 +0800

    Move css for navbar component into assets

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,30 +1,5 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
-<style>
-    nav {
-        background-color: #0f457a;
-        padding: 1rem 0;
-        position: absolute;
-        top: 0;
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-    }
-
-    nav a {
-        color: white;
-        text-decoration: none;
-        margin: 0 15px;
-        font-weight: 600;
-        transition: color 0.3s ease;
-    }
-
-    nav a:hover {
-        color: #bbdefb;
-        /* Slightly lighter blue on hover */
-    }
-</style>
+<link rel="stylesheet" href="{{ asset('css/components/navbar.css') }}">
 
 <nav>
     <a href="#">Home</a>

e47fe97d52b81b52561d962b3f2e157d7a539893
Lorenzo-Mariano <hhumminah@gmail.com>
2024-09-12 20:32:51 +0800

    Create page for "Get Involved" tab of the navbar

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/get-involved', function () {
+    return view('get-involved');
+});
+
 Route::get('/secret', function () {
     return "You aren't supposed to be here.";
 });

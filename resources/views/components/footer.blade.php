003cf0bee3ad1ddd765384adb3b7b30066569b34
Lorenzo-Mariano <hhumminah@gmail.com>
2024-09-23 14:02:06 +0800

    Create footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,50 @@
+@vite(['public/css/components/footer.css'])
+
+<footer>
+    <section class="intro">
+        <div>
+            <h2>Blue Alert</h2>
+            <span>Protecting our rivers, protecting our future</span>
+        </div>
+
+        <ul>
+            <li>
+                <a href="">Privacy Policy</a>
+            </li>
+            <li>
+                <a href="">Terms & Conditions</a>
+            </li>
+        </ul>
+    </section>
+    <section class="quick-links">
+        <div>
+            <h2>Quick Links</h2>
+            <span>Protecting our rivers, protecting our future</span>
+        </div>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/articles">Articles</a>
+            </li>
+            <li>
+                <a href="/infographics">Infographics</a>
+            </li>
+            <li>
+                <a href="/get-involved">Get Involved</a>
+            </li>
+        </ul>
+    </section>
+    <section class="contact-us">
+        <h2>Contact Us</h2>
+        <div class="contacts">
+            <i class="iconoir-mail"></i>
+            <span>bluealert@gmail.com</span>
+            <i class="iconoir-phone"></i>
+            <span>0912345567891</span>
+            <i class="iconoir-map-pin"></i>
+            <span>PHINMA AU South, Maharlika Highway</span>
+        </div>
+    </section>
+</footer>

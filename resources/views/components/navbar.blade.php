68a62c1936dfcdcdeaffd6e2e2d65601fe9daf2a
L0v3rboy <ailvingamana1@gmail.com>
2024-10-01 03:07:38 +0800

    Active nav bar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,9 @@
 @vite(['resources/css/app.css', 'public/css/components/navbar.css'])
 
 <nav>
-    <a href="/">Home</a>
-    <a href="/articles">Articles</a>
-    <a href="/infographics">Infographics</a>
-    <a href="/get-involved">Get Involved</a>
+    <a href="/" class="{{ Request::is('/') ? 'active' : '' }}">Home</a>
+    <a href="/about" class="{{ Request::is('about') ? 'active' : '' }}">About Us</a>
+    <a href="/articles" class="{{ Request::is('articles') ? 'active' : '' }}">Articles</a>
+    <a href="/get-involved" class="{{ Request::is('get-involved') ? 'active' : '' }}">Get Involved</a>
 </nav>
+

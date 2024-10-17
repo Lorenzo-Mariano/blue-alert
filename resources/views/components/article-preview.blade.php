05f0b8feec0f025212aa750b25c7fd3920f3ae16
Lorenzo-Mariano <hhumminah@gmail.com>
2024-10-18 07:29:25 +0800

    Make UI components have white background

--- resources/views/components/article-preview.blade.php
+++ resources/views/components/article-preview.blade.php
@@ -3,6 +3,6 @@
 <div class="blog-preview">
     <a href="/">
         <img src="https://m.media-amazon.com/images/I/81XP3iQ9vML._AC_SL1500_.jpg" alt="blog-image">
-        <h3 class="title">the world if filipinos did not exist</h3>
+        <h3 class="title">Lorem ipsum dolor sit amet consectetur</h3>
     </a>
 </div>

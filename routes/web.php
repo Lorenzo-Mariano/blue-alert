150427407e28408fabaff08769ffd0dc04c7c968
Lorenzo-Mariano <hhumminah@gmail.com>
2024-10-23 12:07:32 +0800

    Add create article page, add full page for individual article with placeholder image

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Storage;
+use App\Models\Article;
 
 // Just pages
 
@@ -10,9 +12,17 @@ Route::get('/', function () {
     return view('pages.welcome');
 });
 
+
 Route::get('/articles', function () {
-    return view('pages.articles');
+    $articles = Article::with('author')->get(); // Fetch all articles with author data
+    $hasArticles = $articles->isNotEmpty(); // Check if there are any articles
+
+    return view('pages.articles', [
+        'articles' => $articles,
+        'hasArticles' => $hasArticles,
+    ]);
 });
+
 
 Route::get('/about-us', function () {
     return view('pages.about-us');
@@ -21,6 +31,13 @@ Route::get('/about-us', function () {
 Route::get('/get-involved', function () {
     return view('pages.get-involved');
 })->name('login');
+
+Route::get('/hail-Mary', function () {
+    $files = Storage::disk('backblaze')->allFiles();
+
+    // Return a view or JSON response with the list of files
+    return response()->json($files);
+});
 
 // Functional stuff
 
@@ -32,6 +49,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/secret', function () {
         return "You aren't supposed to be here.";
     });
+
+    Route::post('/article', [ArticleController::class, 'create']);
 });
 
 Route::post('/register', [UserController::class, 'register']);

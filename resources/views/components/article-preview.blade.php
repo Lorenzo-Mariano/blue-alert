b2f59e811f5c9ce7c8da40d9a0354e98923a1624
Lorenzo-Mariano <hhumminah@gmail.com>
2024-11-11 22:21:58 +0800

    Show more details in the article previews

--- resources/views/components/article-preview.blade.php
+++ resources/views/components/article-preview.blade.php
@@ -5,6 +5,17 @@
         <div class="img-wrapper">
             <img src="{{ Storage::url($article->image) }}" alt="blog-image">
         </div>
-        <h3 class="title">{{ $article->title }}</h3>
+        <section class="text">
+            <h3 class="title">{{ $article->title }}</h3>
+
+            <!-- New Information to Display -->
+            <p class="author">
+                By {{ $article->author->first_name }} {{ $article->author->last_name }} on
+                {{ $article->created_at->format('M d, Y') }}
+            </p>
+            <p class="read-count">
+                Reads: {{ $article->reads }}
+            </p>
+        </section>
     </a>
 </div>

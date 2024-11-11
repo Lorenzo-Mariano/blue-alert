86fa5af7334a64a4fe09b261a5ffe8a3bb72f300
Lorenzo-Mariano <hhumminah@gmail.com>
2024-11-11 17:26:39 +0800

    hindi sabog na articles page

--- resources/views/pages/articles.blade.php
+++ resources/views/pages/articles.blade.php
@@ -14,50 +14,6 @@
     </header>
 
     <main>
-<<<<<<< HEAD
-                
-
-        <section class="blogs">
-
-            <div class="article-preview">
-                <img src="https://via.placeholder.com/300" alt="Article image">
-                <p>Lorem ipsum dolor sit amet, consectetur</p>
-            </div>
-
-            <div class="article-preview">
-                <img src="https://via.placeholder.com/300" alt="Article image">
-                <p>Adipiscing elit, sed do eiusmod tempor incididunt</p>
-            </div>
-
-            <div class="article-preview">
-                <img src="https://via.placeholder.com/300" alt="Article image">
-                <p>Ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
-            </div>
-
-            <div class="article-preview">
-                <img src="https://via.placeholder.com/300" alt="Article image">
-                <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
-            </div>
-
-            <div class="article-preview">
-                <img src="https://via.placeholder.com/300" alt="Article image">
-                <p>Ex ea commodo consequat. Duis aute irure dolor in reprehenderit</p>
-            </div>
-
-            <div class="article-preview">
-                <img src="https://via.placeholder.com/300" alt="Article image">
-                <p>In voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-            </div>
-            
-        </section>
-
-        <section class="pagination">
-            <a href="/" class="pagination-link">01</a>
-            <a href="/" class="pagination-link">02</a>
-            <a href="/" class="pagination-link">03</a>
-            <a href="/" class="pagination-link">04</a>
-        </section>
-=======
         <div class="left-panel">
             @if (Auth::check())
                 @include('components.article-controls')
@@ -84,7 +40,6 @@
             </div>
 
         </div>
->>>>>>> b707c652419470b666c6083dbc0b02b8bd89feb1
     </main>
 
 
